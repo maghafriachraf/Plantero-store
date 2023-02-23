0ebb5df2def7ecc0c058a754a43a4f3c74467d88
achrafmaghafri <maghafriachraf@gmail.com>
2023-02-23 22:59:31 +0100

    update 1

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -4,13 +4,13 @@ import water from '../assets/water.svg'
 
 function handleScale(e,caretype,scaleValue){
   e.stopPropagation();
-  const requiert = scaleValue == 3 ? 'beaucoup' : scaleValue == 2 ? 'modérement' : 'peu'
-  const type = caretype == 'light' ? `de lumière` : `d'arrosage`
+  const requiert = scaleValue === 3 ? 'beaucoup' : scaleValue === 2 ? 'modérement' : 'peu'
+  const type = caretype === 'light' ? `de lumière` : `d'arrosage`
   alert(`Cette plante requiert ${requiert} ${type} 🌱🌱`)
 }
 
 function CareScale({careType, scaleValue}){
-  const scaleType = careType == 'light' ? <img src={sun} alt='sun'/> : <img src={water} alt='water' />
+  const scaleType = careType === 'light' ? <img src={sun} alt='sun'/> : <img src={water} alt='water' />
   const range = [1,2,3]
 
   return ( <div onClick={(e) => handleScale(e,careType,scaleValue)}>
